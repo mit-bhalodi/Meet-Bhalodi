77c9bdfe9d53da93416ec4e58b33cb9dff6b0ed5
Meet Bhalodi <meetbhalodi@Meets-MacBook-Air.local>
2024-02-25 19:20:18 +0530

    Minor UI Improvements

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
                 <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
                     <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 Meet Bhalodi All Rights Reserved.
                     </span>
-                    <a href='https://ui.aceternity.com/'>thanks to aceternity</a>
+                    <a href='https://ui.aceternity.com/' target='_blank' className='text-xs md:text-sm ml-4'>Aceternity</a>
                 </div>
             </footer>
         </div>
